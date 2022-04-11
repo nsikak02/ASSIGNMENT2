2471026b585707fcb2ae17c821ce2601cb2f7c2d
nsikak02 <nsikakdbest@gmail.com>
2022-04-12 02:48:30 +1300

    added a task

---
+++ main.js
@@ -0,0 +1,27 @@
+// let browser = prompt('which browser are you using');
+// if ( browser =='edge') {alert ( 'you have the edge')}
+// else if ( browser == 'chrome') {alert ( 'you have the chrome')}
+// else if (browser == 'firefox') {alert ( 'you have the fire')}
+// else if (browser == 'safari') {alert ( 'you are safe')}
+// else if ( browser == 'opera') {alert ( 'you are in the era')}
+// else {alert ( 'Sorry')};
+// console.log(browser)
+
+
+
+let a = +prompt('a?', '');
+
+switch (a) {
+  case 0:
+    alert( 0 );
+    break;
+
+  case 1:
+    alert( 1 );
+    break;
+
+  case 2:
+  case 3:
+    alert( '2,3' );
+    break;
+}
